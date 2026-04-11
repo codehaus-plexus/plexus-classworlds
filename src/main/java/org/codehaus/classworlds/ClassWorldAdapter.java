223416e73fb4b9f043d877b06dab43b5b80e0501
Sylwester Lachiewicz <slachiewicz@apache.org>
2026-04-11 11:31:00 +0200

    Revert "Drop deprecated package org.codehaus.classworlds"
    This reverts commit ff94d8ac

---
+++ src/main/java/org/codehaus/classworlds/ClassWorldAdapter.java
@@ -0,0 +1,83 @@
+package org.codehaus.classworlds;
+
+/*
+ * Copyright 2001-2010 Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Collection;
+import java.util.Vector;
+
+/**
+ * An adapter for ClassWorlds
+ *
+ * @author Andrew Williams
+ */
+@SuppressWarnings("rawtypes")
+@Deprecated
+public class ClassWorldAdapter extends ClassWorld {
+
+    public static ClassWorldAdapter getInstance(org.codehaus.plexus.classworlds.ClassWorld newWorld) {
+        return new ClassWorldAdapter(newWorld);
+    }
+
+    private final org.codehaus.plexus.classworlds.ClassWorld world;
+
+    private ClassWorldAdapter(org.codehaus.plexus.classworlds.ClassWorld newWorld) {
+        super(false);
+        this.world = newWorld;
+    }
+
+    public ClassRealm newRealm(String id) throws DuplicateRealmException {
+        try {
+            return ClassRealmAdapter.getInstance(world.newRealm(id));
+        } catch (org.codehaus.plexus.classworlds.realm.DuplicateRealmException e) {
+            throw new DuplicateRealmException(this, e.getId());
+        }
+    }
+
+    public ClassRealm newRealm(String id, ClassLoader classLoader) throws DuplicateRealmException {
+        try {
+            return ClassRealmAdapter.getInstance(world.newRealm(id, classLoader));
+        } catch (org.codehaus.plexus.classworlds.realm.DuplicateRealmException e) {
+            throw new DuplicateRealmException(this, e.getId());
+        }
+    }
+
+    public void disposeRealm(String id) throws NoSuchRealmException {
+        try {
+            world.disposeRealm(id);
+        } catch (org.codehaus.plexus.classworlds.realm.NoSuchRealmException e) {
+            throw new NoSuchRealmException(this, e.getId());
+        }
+    }
+
+    public ClassRealm getRealm(String id) throws NoSuchRealmException {
+        try {
+            return ClassRealmAdapter.getInstance(world.getRealm(id));
+        } catch (org.codehaus.plexus.classworlds.realm.NoSuchRealmException e) {
+            throw new NoSuchRealmException(this, e.getId());
+        }
+    }
+
+    public Collection getRealms() {
+        Collection<org.codehaus.plexus.classworlds.realm.ClassRealm> realms = world.getRealms();
+        Vector<ClassRealmAdapter> ret = new Vector<>();
+        for (org.codehaus.plexus.classworlds.realm.ClassRealm classRealm : realms) {
+            ret.add(ClassRealmAdapter.getInstance(classRealm));
+        }
+
+        return ret;
+    }
+}
---
+++ src/main/java/org/codehaus/classworlds/ClassWorldReverseAdapter.java
@@ -0,0 +1,95 @@
+package org.codehaus.classworlds;
+
+/*
+ * Copyright 2001-2010 Codehaus Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Vector;
+
+/**
+ * A reverse adapter for ClassWorlds
+ *
+ * @author Andrew Williams
+ */
+@SuppressWarnings({"unchecked", "rawtypes"})
+@Deprecated
+public class ClassWorldReverseAdapter extends org.codehaus.plexus.classworlds.ClassWorld {
+    private static final HashMap instances = new HashMap();
+
+    public static ClassWorldReverseAdapter getInstance(ClassWorld oldWorld) {
+        if (instances.containsKey(oldWorld)) return (ClassWorldReverseAdapter) instances.get(oldWorld);
+
+        ClassWorldReverseAdapter adapter = new ClassWorldReverseAdapter(oldWorld);
+        instances.put(oldWorld, adapter);
+
+        return adapter;
+    }
+
+    private final ClassWorld world;
+
+    private ClassWorldReverseAdapter(ClassWorld newWorld) {
+        super();
+        this.world = newWorld;
+    }
+
+    public org.codehaus.plexus.classworlds.realm.ClassRealm newRealm(String id)
+            throws org.codehaus.plexus.classworlds.realm.DuplicateRealmException {
+        try {
+            return ClassRealmReverseAdapter.getInstance(world.newRealm(id));
+        } catch (DuplicateRealmException e) {
+            throw new org.codehaus.plexus.classworlds.realm.DuplicateRealmException(this, e.getId());
+        }
+    }
+
+    public org.codehaus.plexus.classworlds.realm.ClassRealm newRealm(String id, ClassLoader classLoader)
+            throws org.codehaus.plexus.classworlds.realm.DuplicateRealmException {
+        try {
+            return ClassRealmReverseAdapter.getInstance(world.newRealm(id, classLoader));
+        } catch (DuplicateRealmException e) {
+            throw new org.codehaus.plexus.classworlds.realm.DuplicateRealmException(this, e.getId());
+        }
+    }
+
+    public void disposeRealm(String id) throws org.codehaus.plexus.classworlds.realm.NoSuchRealmException {
+        try {
+            world.disposeRealm(id);
+        } catch (NoSuchRealmException e) {
+            throw new org.codehaus.plexus.classworlds.realm.NoSuchRealmException(this, e.getId());
+        }
+    }
+
+    public org.codehaus.plexus.classworlds.realm.ClassRealm getRealm(String id)
+            throws org.codehaus.plexus.classworlds.realm.NoSuchRealmException {
+        try {
+            return ClassRealmReverseAdapter.getInstance(world.getRealm(id));
+        } catch (NoSuchRealmException e) {
+            throw new org.codehaus.plexus.classworlds.realm.NoSuchRealmException(this, e.getId());
+        }
+    }
+
+    public Collection getRealms() {
+        Collection realms = world.getRealms();
+        Vector ret = new Vector();
+
+        for (Object o : realms) {
+            ClassRealm realm = (ClassRealm) o;
+            ret.add(ClassRealmReverseAdapter.getInstance(realm));
+        }
+
+        return ret;
+    }
+}
